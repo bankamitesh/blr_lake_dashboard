f28066d546e70f28228d8e33dfa607a1126df920
Rajeev Jha <jha.rajeev@gmail.com>
2017-01-08 15:27:18 +0530

    fixed bathymetry page + contour plot data in Gnuplot style

--- web/admin/lake/bathymetry.php
+++ web/admin/lake/bathymetry.php
@@ -23,84 +23,105 @@
     if (array_key_exists("jsdebug", $_REQUEST)) {
         $gparams->debug = true;
     }
-
+    
 ?>
 
 
 <html  ng-app="YuktixApp">
 <head>
-    <title> Lake stage volume edit page </title>
+    <title> Lake Bathymetry data page </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/mdl/material.min.css" />
     <link rel="stylesheet" href="/assets/mdl/material.light_green-pink.min.css" />
-    <link rel="stylesheet" href="/assets/css/main.css?v=1" />
+    <link rel="stylesheet" href="/assets/css/main.css?v=4" />
+
+    <style>
+      
+
+    </style>
 
 </head>
 
 <body  ng-controller="yuktix.admin.lake.csv.upload">
 
-<div class="mdl-layout mdl-js-layout" id="container">
+    <div class="mdl-layout mdl-js-layout">
+        <?php include(APP_WEB_DIR . '/inc/ui/mdl-header.inc'); ?>
+        <?php include(APP_WEB_DIR . '/inc/ui/mdl-drawer.inc'); ?>
+        
+        <main class="mdl-components__pages mdl-layout__content ">
+            <?php include(APP_WEB_DIR . '/inc/ui/mdl-progress.inc'); ?>
 
-    <?php include(APP_WEB_DIR . '/inc/ui/mdl-header.inc'); ?>
-    <?php include(APP_WEB_DIR . '/inc/ui/mdl-drawer.inc'); ?>
-   
-    <main class="mdl-components__pages mdl-layout__content ">
-        <div class="mdl-grid">
-            <?php include(APP_WEB_DIR . '/inc/ui/mdl-edit-sidebar.inc'); ?>
-            <div class="mdl-cell mdl-cell--1-col"> </div>
-            <div  class="mdl-cell mdl-cell--6-col container-810" >
-                <?php include(APP_WEB_DIR . '/inc/ui/mdl-page-message.inc'); ?>
-                
-                   <form name="stageVolumeForm" >
-                        
-                            <h5>Upload stage volume data </h5>
-                           
+                <div class="mdl-grid">
+                    <?php include(APP_WEB_DIR . '/inc/ui/mdl-edit-sidebar.inc'); ?>
+                    <div class="mdl-cell mdl-cell--1-col"> </div>
+                    <div  class="mdl-cell mdl-cell--6-col container-810" >
+                        <?php include(APP_WEB_DIR . '/inc/ui/mdl-page-message.inc'); ?>
+                        <form name="csvUploadForm" >
+                          
                             <p>
-                            Please upload the lake stage volume data in CSV format.
-                            *add a sample here *
+                            Upload data for lake bathymetry. For contour plotting, 
+                            The file should contain [x ,y, f(x,y)] tuples like shown 
+                            below in sample.
                             </p>
+                             <h5> sample </h5>
+                             <ul class="mdl-list">
+                                <li class="mdl-list__item small-sample" ng-repeat="sample in samples">
+                                    <span class="mdl-list__item-primary-content" ng-bind="sample">  </span>    
+                                </li>
+                            </ul>
 
-                            <div>
+                            <div class="upload-button-container">
                                 <label class="mdl-button mdl-button--colored mdl-js-button">
-                                    <span> <i class="material-icons">attachment</i> </span>
+                                    <span> <i class="material-icons">attach_file</i> </span>
                                     Select File<input type="file" filelist-bind class="none"  name="files" style="display: none;">
                                 </label>
                             </div>
-                            <br>
+                            
                             <div>
                                 <ul class="mdl-list">
-                                    <li "mdl-list__item" ng-repeat="file in files">
+                                    <li class="mdl-list__item mdl-list__item--two-line" ng-repeat="file in files">
                                         <span class="mdl-list__item-primary-content">
-                                            <i class="material-icons mdl-list__item-icon">insert_drive_file</i>
-                                            {{ file.name}}, {{file.size/1000}} kb
+                                            
+                                            <span> {{ file.name}} </span>
+                                            <span class="mdl-list__item-sub-title">{{file.size/1000}} kb</span>
+                                            
                                         </span>
-                                    
+                                        <span class="mdl-list__item-secondary-content">
+                                            <i class="material-icons">check</i>
+                                        </span>
+
                                     </li>
                                 </ul>
                             </div>
 
-                            <div class="form-button-container">
-                                <button class="mdl-button mdl-js-button mdl-button--raised"ng-click="process_upload()" type="submit">
+                            <div class="upload-button-container" ng-show="files.length > 0 ">
+                                <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" ng-click="process_upload()" type="submit">
                                     Upload 
                                 </button>
                             </div>
                         </form> 
+                       
+                        <div class="file-download-container" ng-show="display.downloadLink">
+                            <h6>System Bathymetry data file</h6>
+                            <a ng-href="{{base}}/admin/shim/download/file.php?id={{lakeFileObj.fileId}}">
+                                <span> {{lakeFileObj.fileName}} ( {{lakeFileObj.createdOn}} )</span>
+                                <i class="material-icons">file_download</i>
+                                
+                            </a>
+                        </div>
+
+                        <div ng-show="!display.downloadLink">
+                            <h6>No Bathymetry data file in the system </h6>
+                        </div>
                         
-                    <div ng-show="display.downloadLink">
-                        <h6>Download stored stage volume file </h6>
-                        <a ng-href="{{base}}/admin/shim/download/file.php?id={{lakeFileObj.fileId}}">
-                            <i class="material-icons mdl-list__item-icon">file_download</i>
-                            <span> click to download csv</span>
-                        </a>
-                    </div>
-                    <div ng-show="!display.downloadLink">
-                        <h6>No stage volume file stored in the system </h6>
-                    </div>
-                    
+                        <div style="padding-top:41px;">
+                        Add preview here 
+                        </div> <!-- preview -->
 
                 </div>
         </div> <!-- grid:content -->
-        
+       
+       
         <div class="mdl-grid mdl-grid--no-spacing">
             <div class="mdl-cell mdl-cell--12-col">
                 <?php include(APP_WEB_DIR . '/inc/ui/mdl-footer.inc'); ?>
@@ -110,7 +131,8 @@
 
     </main>
     
-    </div> 
+    
+ </div> 
 </body>
 
 
@@ -163,7 +185,7 @@
 
                     if (status != 200 || data.code != 200) {
                         console.log(response);
-                        var error = data.error || (status + ":error retrieving  data from Server");
+                        var error = data.error || (status + ":error retrieving  data from server");
                         $scope.showError(error);
                         return;
                     }
@@ -176,6 +198,7 @@
                     // set display.downloadLink
                     if($scope.lakeFileObj.hasOwnProperty("fileId")) {
                         $scope.display.downloadLink = true ;
+                        $scope.lakeFileObj.createdOn =  new Date($scope.lakeFileObj.tsUnix * 1000).toLocaleString() ;
                     }
                     
                     $scope.clearPageMessage();
@@ -208,6 +231,8 @@
             formData.append("metadata", angular.toJson(metadata));
             
             $scope.showProgress("uploading file...");
+
+            // resolve file upload promise
             fupload.send_form_data($scope.debug, uploadUrl, formData).then(function (response) {
 
                 var status = response.status || 500;
@@ -221,8 +246,8 @@
                 if (status != 200 || data.code != 200) {
                     console.error("browser response object: " ,response);
                     var error  = data.error || (status + ":error while submitting data ");
-                    // show error 
                     $scope.showError(error);
+                    $scope.showToastMessage(error);
                     return ;
                 }
                 
@@ -249,15 +274,15 @@
 
                     if (status != 200 || data.code != 200) {
                         console.log("browser response object: %o" ,response);
-                        var error = data.error || (status + ":error submitting feature create form");
+                        var error = data.error || (status + ":error submitting bathymetry data");
                         $scope.showError(error);
+                        $scope.showToastMessage(error);
                         return;
                     }
 
-                    $scope.showMessage("lake file data uploaded successfully!");
-                    // @debug
-                    // reload page
-                    $window.location.href = "/admin/lake/stage-volume.php?lake_id=" + $scope.lakeId ;
+                    var message = "lake bathymetry data uploaded!" 
+                    $scope.showToastMessage(message);
+                    $scope.clearPageMessage();
 
                 }, function (response) {
                     $scope.processResponse(response);
@@ -287,15 +312,37 @@
         $scope.display = {} ;
         $scope.display.downloadLink = false ;
 
-        // lake edit menu display 
+         // lake edit menu display 
         $scope.display.lakeEditMenu = {} ;
         $scope.display.lakeEditMenu.bathymetry = true ;
+
 
         // file code: 1 stage-volume
         // file code: 2 stage-area
         // file code: 3 evaporation
+        // file code: 4 bathymetry
 
-        $scope.fileCode = 1 ;
+        $scope.fileCode = 4 ;
+
+        // sample contour plot data 
+        $scope.samples = [] ;
+        $scope.samples.push("x0 y0 f(x0,y0)");
+        $scope.samples.push("x1 y0 f(x1,y0)") ;
+        $scope.samples.push("x2 y0 f(x2,y0)");
+        $scope.samples.push("...") ;
+
+        $scope.samples.push("xn y0 f(xn,y0)");
+        $scope.samples.push("x0 y1 f(x0,y1) ") ;
+        $scope.samples.push("....") ;
+
+        $scope.samples.push("xn y1 f(xn,y1)");
+        $scope.samples.push(".....") ;
+
+        $scope.samples.push("x0 ym f(x0,ym) ") ;
+        $scope.samples.push(".............") ;
+        $scope.samples.push("xn ym f(xn,ym)") ;
+
+        // start 
         $scope.get_lake_object() ;
     
 
